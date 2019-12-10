bf98af9853d424103313df999b772eb82a5d13a9
Pogorelov Roman <rmk140987@gmail.com>
2019-12-10 17:00:37 +0300

    Spring Boot - Add Post method

--- src/main/java/com/veselov/alex/controller/SurveyController.java
+++ src/main/java/com/veselov/alex/controller/SurveyController.java
@@ -3,10 +3,11 @@ package com.veselov.alex.controller;
 import com.veselov.alex.model.Question;
 import com.veselov.alex.service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,6 +18,17 @@ public class SurveyController {
     @GetMapping("/surveys/{surveyId}/questions")
     public List<Question> getQuestions(@PathVariable String surveyId) {
         return this.service.retrieveQuestions(surveyId);
+    }
+
+    @PostMapping("/surveys/{surveyId}/questions")
+    public ResponseEntity<Object> postQuestionToSurvey(@PathVariable String surveyId, @RequestBody Question question) {
+        Question created = this.service.addQuestion(surveyId, question);
+        if (created == null) {
+            return ResponseEntity.noContent().build();
+        }
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping("/surveys/{surveyId}/questions/{questionId}")
